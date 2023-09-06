0a1ba190b927806da673726049e2a4fac968ca40
ohsang <ast6108@naver.com>
2023-09-06 22:00:23 +0900

    requiered

--- JavaScript/app.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector(".hello:first-child h1");
-
-function handleTitleClick() {
-  title.style.color = "blue";
-}
-
-title.addEventListener("click", handleTitleClick);
